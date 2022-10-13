e85b96f3668978f73225d2014a8b4e69e2eaecdc
beast-codes <eaglestradingconsult@gmail.com>
2022-10-13 16:02:04 -0700

    print_with_put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ * Return -always zero if there are no errors
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
