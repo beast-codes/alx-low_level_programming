669c198a30332933f2ec8b706f52c49a4913b81e
beast-codes <eaglestradingconsult@gmail.com>
2022-10-17 16:21:51 -0700

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabet in lowerdase
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+putchar(lc);
+putchar('\n');
+return (0);
+}
