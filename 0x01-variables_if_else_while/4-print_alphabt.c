058a0efc74d2aa55ef013a30555de31abe7d77d5
beast-codes <eaglestradingconsult@gmail.com>
2022-10-17 16:43:55 -0700

    d

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,8 @@
 int main(void)
 {
   char lc;
-  for (lc = 'a'; lc <= 'z'; lc++)
-    putchar(lc);
-  if (lc != 'e' && lc != 'q')
+for (lc = 'a'; lc <= 'z'; lc++)
+if (lc != 'e' && lc != 'q')
     putchar(lc);
   putchar('\n');
   return (0);
