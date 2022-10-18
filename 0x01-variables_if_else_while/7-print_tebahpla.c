5e06b4b2c433b5082e68a0898c5532ec70da7d0d
beast-codes <eaglestradingconsult@gmail.com>
2022-10-17 17:27:44 -0700

    d

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabet in lowercase backwards
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+char lc;
+for (lc = 'z'; lc <= 'a'; lc--)
+putchar(lc);
+putchar('\n');
+return (0);
+}
