0e2d0c81c9ac19dd956f5485eb576e0b3e8cafc9
beast-codes <eaglestradingconsult@gmail.com>
2022-10-17 17:00:39 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - code body
+ *
+ * Details: Print number all base 10 number beginning with 0
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
